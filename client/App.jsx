2d713d8a27500353c68be834dd23ec18994f254a
Kunal Patel <kunalpatel73@gmail.com>
2018-03-06 11:50:24 -0800

    updated basic app structure within multiple container files

--- client/App.jsx
+++ client/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import HeaderContainer from './containers/HeaderContainer.jsx';
 import RouteContainer from './containers/RouteContainer.jsx';
 import RouteListContainer from './containers/RouteListContainer.jsx';
-import RouteTimelineContainer from './containers/RouteTimelineContainer.jsx';
 
 class App extends Component {
     constructor(props) {
@@ -15,7 +14,6 @@ class App extends Component {
                 <HeaderContainer />
                 <RouteContainer />
                 <RouteListContainer />
-                <RouteTimelineContainer />
             </div>
         )
     }
