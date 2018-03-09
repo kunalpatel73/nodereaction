57a82fdebcd50259b247bd2f2b9f4f59ae5f042c
Michael Dalton <modalton@usc.edu>
2018-03-08 21:50:45 -0800

    speakers

--- client/components/HeaderLogo.js
+++ client/components/HeaderLogo.js
@@ -1,25 +1,24 @@
-<<<<<<< HEAD
-=======
-import React from 'react';
+import React from "react";
 
-const HeaderLogo = (props) => {
-    return (
-        <div>
-         <img style={styles.headerLogo} src='https://honeycomb.io/images/integrations/nginx.png' />
-        </div>
-    )
-}
+const HeaderLogo = props => {
+  return (
+    <div>
+      <img
+        style={styles.headerLogo}
+        src="https://honeycomb.io/images/integrations/nginx.png"
+      />
+    </div>
+  );
+};
 
 const styles = {
-    headerLogo: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '80px',
-        height: '80px'
-    }
-}
+  headerLogo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "80px",
+    height: "80px"
+  }
+};
 
 export default HeaderLogo;
->>>>>>> f484a3bb8322690dc70b8d81ca8e22eeae2f0f8f
-
