5859d2eebdf90423f5b789e78f450fa63968ed40
Chris Jeon <chrisjwas@gmail.com>
2018-03-08 17:56:50 -0800

    move worldfactbook

---
+++ testapps/doggie/server.js
@@ -0,0 +1,30 @@
+const NRA = require('../agent/NodeReaction');
+
+const express = require("express");
+const app = express();
+const dogController = require("./controllers/dataController");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "./../"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/dogs", dogController.getDogs, function(req, res) {
+  res.send("shouldnt hit");
+});
+
+app.post("/dogs", dogController.addDog, function(req, res) {
+  res.send("shouldnt hit");
+});
+
+app.post("/serverdata", (req, res) => {
+    console.log("PACKET INCOMING");
+    console.log(req.body.packet);
+    res.send("thank ya kindly");
+   });
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`PORT ${PORT} is listening`);
+});
